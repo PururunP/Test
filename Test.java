ce6d886d8af72dcb1fba3d8e61fcfe33730db107
PururunP <pururunp@gmail.com>
2021-07-02 23:39:45 +0900

    printHelloメソッドの追加

--- Test.java
+++ Test.java
@@ -2,4 +2,7 @@ public class Test{
     public static void main(String[] args){
        System.out.print("Test");
     }
+    public static void printHello() {
+    	System.out.println("Hello");
+    }
 }
