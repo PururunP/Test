c90226efc176a1a71f98c3abab71eac46f59b0df
PururunP <pururunp@gmail.com>
2021-07-03 00:07:12 +0900

    変更その１

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test{
     public static void main(String[] args){
        System.out.println("Test");
        printHello();
+       printHello();
     }
     public static void printHello() {
     	System.out.print("Hello");
