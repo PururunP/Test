bb7a0f78936125e186ff54d339e0852a66018113
PururunP <pururunp@gmail.com>
2021-07-03 00:12:05 +0900

    コミット2

--- Test.java
+++ Test.java
@@ -2,13 +2,12 @@ public class Test{
     public static void main(String[] args){
        System.out.println("Test");
        printHello();
-       printHello();
     }
     public static void printHello() {
-    	System.out.print("Hello");
-    	System.out.print("Hello");
-    	System.out.print("Hello");
-    	System.out.print("Hello");
+    	System.out.println("Hello");
+    	System.out.println();
+    	System.out.println("Hello");
+    	System.out.println("Hello");
     	System.out.println("Hello");
     }
 }
